a75d805b406fd296de5c330b93899921f0946260
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2024-07-15 20:46:19 +0400

    Add Lambda expressions

--- C#/LinqApp/Program.cs
+++ C#/LinqApp/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-int[] nums = [1, 2, 5, -100, 3, 0];
+﻿//int[] nums = [1, 2, 5, -100, 3, 0];
 
 //var resultNums = nums.Append(10);
 
@@ -19,6 +17,8 @@ int[] nums = [1, 2, 5, -100, 3, 0];
 //Console.WriteLine(nums.Count());
 //Console.WriteLine(nums.Length);
 
+
+/*
 bool IsPositive(int num)
 {
     return num >= 0;
@@ -38,3 +38,91 @@ foreach (var item in positiveNumbers)
 {
     Console.WriteLine(item);
 }
+*/
+
+
+/*
+void MyMethod(int num)
+{
+    Console.WriteLine($"method {num}");
+}
+
+Action<int> action = new Action<int>(MyMethod);
+action += MyMethod;
+action += delegate (int num)
+{
+    Console.WriteLine($"delegate {num}");
+};
+action += (num) => Console.WriteLine($"lambda {num}");
+action += (num) =>
+{
+    Console.WriteLine($"lambda 1 line: {num}");
+    Console.WriteLine($"lambda 2 line: {num}");
+};
+
+action.Invoke(123);
+*/
+
+/*
+int total = 0;
+
+Action<bool, int> action = (check, num) =>
+{
+    total += check ? num : -num;
+};
+
+Console.WriteLine(total);
+action(true, 20);
+action(false, 15);
+Console.WriteLine(total);
+*/
+
+
+
+//Func<Action<string>, bool> func = delegate (Action<string> action) { return true; };
+//Func<Action<string>, bool> func = (action) => true;
+//Func<Action<string>, bool> func = (action) =>
+//{
+//    action.Invoke("func lambda");
+//    return true;
+//};
+
+/*
+Func<Action<string>, string, bool> func = (action, msg) =>
+{
+    action.Invoke(msg);
+    return true;
+};
+
+func.Invoke((msg) => Console.WriteLine($"Message from action: {msg}"), "Hello");
+*/
+
+
+
+//Action<Predicate<int>, int> printIf = (checkNum, num) => Console.WriteLine(checkNum(num) ? num : null);
+
+//Predicate<int> checkNum = (num) => num % 2 == 1;
+
+//printIf(checkNum, 17);
+//printIf(checkNum, 16);
+
+
+
+/*
+var result = nums.Where(num => num % 2 == 1);
+
+//List<int> list2 = new List<int>();
+List<int> list1 = result.ToList();
+
+list1.ForEach(num => Console.WriteLine(num));
+*/
+
+//nums
+//    .Where(num => num % 2 == 1)
+//    .ToList()
+//    .ForEach(num => Console.WriteLine(num));
+
+
+
+IEnumerable<string> words = [ "something" ];
+var containsBadword = words.Any(word => word.ToLower().Contains("badword"));
