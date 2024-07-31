d73cd139f7f8442ab661392c1bff59cce27e31bf
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2024-07-31 21:40:42 +0400

    Add subqueries

--- Sql/build-in functions.sql
+++ Sql/build-in functions.sql
@@ -75,3 +75,58 @@ where len(Name) >= 4
 select *
 from Products
 where Price > avg(Price)
+
+
+
+create table Users(
+	[Name] varchar(50) not null,
+	[Fincode] varchar(7) not null unique check(len([Fincode]) = 7),
+	[IsMarried] bit,
+	[BirthDate] datetime2
+)
+ 
+insert into Users([Name], [Fincode], [IsMarried], [BirthDate])
+values ('John', 'ASDA777' , 0, '01-01-1991'),
+('Test', 'ASDAS34' , 0, '01-02-2017'),
+('Bob', 'AZ34567' , 1, '10-10-2004'),
+('Ann', 'ZY34568', 0, '10-10-2005'),
+('Kate', 'YZ34569', 1, '10-10-2000')
+
+
+
+-- количество людей в браке
+select count(*) from Users where IsMarried = 1
+-- количество людей, у которых в именах есть дефис
+select count(*) from Users where Name like '%-%'
+-- дата рождения самого старшего человека
+select min(BirthDate) from Users
+-- имена людей, у которых в финкоде есть буква Z и Y
+select Name from Users where Fincode like '%Z%' and Fincode like '%Y%'
+
+
+
+-- список людей, у которых длина имени превышает среднюю арифметическую длину имен
+
+
+select *
+from Users
+where len(Name) > (select avg(len(Name)) from Users)
+
+
+-- необходимо вывести список всех людей, 
+-- у которых дата рождения больше минимальной даты рождения максимум на 10 лет
+
+--datediff
+--dateadd
+select *
+from Users 
+--where BirthDate >= (select min(BirthDate) from Users)
+where BirthDate <= dateadd(year, 10, (select min(BirthDate) from Users))
+
+
+-- min: 1956-10-10
+
+-- OK: 1957-10-10
+-- OK: 1959-02-11
+-- OK: 1966-10-10
+-- NOT OK: 1972-05-23
