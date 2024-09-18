3d708f2e2f8997ac95c293d37a58c00be12d03d0
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2024-09-18 20:50:38 +0400

    Add Threads

--- System Programming/ThreadsApp/Program.cs
+++ System Programming/ThreadsApp/Program.cs
@@ -66,7 +66,7 @@ Console.ReadKey();
 //[1234567890] -> 13
 
 
-
+/*
 int[] nums =
 {
     1,2,3,4,5,6,7,8,9,10
@@ -106,3 +106,116 @@ if (false)
         Console.WriteLine();
     }
 }
+*/
+
+//new Timer((state) =>
+//{
+//    Console.WriteLine("TIMER");
+//}, null, 0, 500);
+
+//Console.ReadKey();
+
+
+
+//new Thread(() =>
+//{
+//    for (int i = 0; i < 10; i++)
+//    {
+//        Thread.Sleep(500);
+//        Console.WriteLine("THREAD");
+//    }
+//})
+//{
+//    IsBackground = false
+//}.Start();
+
+//Console.WriteLine("END");
+
+
+
+/*
+var thread1 = new Thread(() =>
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("THREAD 1");
+    }
+});
+
+Console.WriteLine("START");
+
+thread1.Start();
+
+Thread.Sleep(2000);
+Console.WriteLine("DO WORK 1");
+Thread.Sleep(500);
+Console.WriteLine("DO WORK 2");
+Thread.Sleep(700);
+Console.WriteLine("DO WORK 3");
+
+thread1.Join();
+
+Console.WriteLine("DO WORK 4");
+Console.WriteLine("END");
+*/
+
+/*
+int GetRandomNumber()
+{
+    Thread.Sleep(3000);
+    return Random.Shared.Next(10000);
+}
+
+int result = default;
+
+var thread1 = new Thread(() =>
+{
+    result = GetRandomNumber();
+});
+
+thread1.Start();
+
+Console.WriteLine("Welcome to my app");
+Console.WriteLine("Press any ket to get random Number: ");
+
+Console.ReadKey();
+thread1.Join();
+
+Console.WriteLine(result);
+*/
+
+
+
+
+using ThreadsApp;
+
+Thread GetTheOldestUser(User user)
+{
+    Thread thread = new Thread(() =>
+    {
+        Thread.Sleep(2000);
+
+        var foundUser = new User
+        {
+            Id = 1,
+            Name = "Bob",
+            Surname = "Marley",
+        };
+
+        user.Id = foundUser.Id;
+        user.Name = foundUser.Name;
+        user.Surname = foundUser.Surname;
+    });
+
+    thread.Start();
+
+    return thread;
+}
+
+User user = new User();
+Thread getTheOldestUserThread = GetTheOldestUser(user);
+
+getTheOldestUserThread.Join();
+
+Console.WriteLine(user);
---
+++ System Programming/ThreadsApp/User.cs
@@ -0,0 +1,19 @@
+﻿namespace ThreadsApp;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public User()
+    {
+        this.Name = "UNKNOWN";
+        this.Surname = "UNKNOWN";
+    }
+
+    public override string ToString()
+    {
+        return $"{Id}: {Name} {Surname}";
+    }
+}
