fde0593621dfb427ca7ad542ed7d582f682760c0
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2024-09-18 21:47:50 +0400

    Add ThreadPool

---
+++ System Programming/ThreadPoolApp/Program.cs
@@ -0,0 +1,64 @@
+﻿//Thread thread = new Thread(() =>
+//{
+//    while (true)
+//    {
+//        Console.WriteLine("asdasdasd");
+//    }
+//});
+//thread.Start();
+
+//thread.Abort();
+
+//WaitCallback callback = (obj) =>
+//{
+//    Console.WriteLine(obj);
+//};
+
+//ThreadPool.QueueUserWorkItem(callback, 123);
+
+//Console.ReadKey();
+
+
+
+//ThreadPool.GetMaxThreads(out int t1, out int t2);
+//Console.WriteLine(t1);
+//Console.WriteLine(t2);
+//ThreadPool.SetMaxThreads(2, 2);
+
+/*
+ThreadPool.QueueUserWorkItem((obj) =>
+{
+    Console.WriteLine($"THREAD 1 start: {Thread.CurrentThread.ManagedThreadId}");
+    Thread.Sleep(1000);
+    Console.WriteLine($"THREAD 1 end: {Thread.CurrentThread.ManagedThreadId}");
+});
+
+ThreadPool.QueueUserWorkItem((obj) =>
+{
+    Console.WriteLine($"THREAD 2: {Thread.CurrentThread.ManagedThreadId}");
+});
+
+Console.ReadKey();
+
+ThreadPool.QueueUserWorkItem((obj) =>
+{
+    Console.WriteLine($"THREAD 3: {Thread.CurrentThread.ManagedThreadId}");
+});
+
+Console.ReadKey();
+*/
+
+using ThreadPoolApp;
+
+User user = new User();
+
+ThreadPool.QueueUserWorkItem<User>((state) =>
+{
+    state.Id = 123;
+    state.Name = "CHANGED";
+    state.Surname = "CHANGED";
+}, user, true);
+
+Console.ReadKey();
+
+Console.WriteLine(user);
---
+++ System Programming/ThreadPoolApp/User.cs
@@ -0,0 +1,19 @@
+﻿namespace ThreadPoolApp;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public User()
+    {
+        this.Name = "UNKNOWN";
+        this.Surname = "UNKNOWN";
+    }
+
+    public override string ToString()
+    {
+        return $"{Id}: {Name} {Surname}";
+    }
+}
