0f4706e05ed2f1ce2787aac39fa48f8f7bfe51a9
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2024-09-20 21:50:10 +0400

    Add Json

---
+++ C#/JsonApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using JsonApp;
+using System.Text.Json;
+
+/*
+var user1 = new User
+{
+    Id = 1,
+    Name = "Bob",
+    Surname = "Marley",
+};
+
+var user2 = new User
+{
+    Id = 2,
+    Name = "Ann",
+    Surname = "Marley",
+};
+
+user1.Partner = user2;
+//user2.Partner = user1;
+
+var userJson = JsonSerializer.Serialize(user1);
+Console.WriteLine(userJson);
+
+File.WriteAllText("users.json", userJson);
+*/
+
+
+
+//var userJson = File.ReadAllText("users.json");
+//var user = JsonSerializer.Deserialize<User>(userJson);
+
+//Console.WriteLine(user);
