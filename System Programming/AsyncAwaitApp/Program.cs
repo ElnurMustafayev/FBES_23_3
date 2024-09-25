1451079fa0b235652d960422402d15e701550478
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2024-09-25 22:01:22 +0400

    Add async-await, StateMachine

---
+++ System Programming/AsyncAwaitApp/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static async Task MyMethodAsync()
+    {
+        await Task.Run(() =>
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("Task.Run");
+        });
+    }
+
+    private static async Task Loop()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            await MyMethodAsync();
+        }
+    }
+
+    private static async Task Main(string[] args)
+    {
+        await Loop();
+
+        while (true)
+        {
+            Thread.Sleep(300);
+            Console.WriteLine("MAIN");
+        }
+
+        //Console.WriteLine("Start");
+        //await MyMethodAsync();
+        //MyMethodAsync().Wait();
+        //MyMethodAsync().RunSynchronously();
+        //MyMethodAsync();
+        //Console.WriteLine("End");
+    }
+}
