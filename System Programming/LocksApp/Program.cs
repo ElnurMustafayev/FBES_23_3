b64c187ff5069db5f47a24a99851936bb892d89a
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2024-09-25 20:18:18 +0400

    Add Locks, Monitor, Mutex, Semaphore

--- System Programming/LocksApp/Program.cs
+++ System Programming/LocksApp/Program.cs
@@ -1,27 +1,49 @@
 ﻿#pragma warning disable CS4014
 
 int i = 0;
+//Mutex mutex = new Mutex(false, "MyLockingMutex");
+//Semaphore semaphore = new Semaphore(1,5);
 
-Task.Run(() =>
+object locker = 0;
+
+var task1 = Task.Run(() =>
 {
     Console.WriteLine("Thread 1 start");
     for (int j = 0; j < 10_000_000; j++)
     {
-        i++;
+        lock(locker)
+        {
+            //Monitor.Enter(locker);
+            //semaphore.WaitOne();
+            //mutex.WaitOne();
+            i++;
+            //mutex.ReleaseMutex();
+            //semaphore.Release();
+            //Monitor.Exit(locker);
+        }
     }
     Console.WriteLine("Thread 1 end");
 });
 
-Task.Run(() =>
+var task2 = Task.Run(() =>
 {
     Console.WriteLine("Thread 2 start");
     for (int j = 0; j < 10_000_000; j++)
     {
-        i++;
+        lock (locker)
+        {
+            //Monitor.Enter(locker);
+            //semaphore.WaitOne();
+            //mutex.WaitOne();
+            i++;
+            //mutex.ReleaseMutex();
+            //semaphore.Release();
+            //Monitor.Exit(locker);
+        }
     }
     Console.WriteLine("Thread 2 end");
 });
 
-Console.ReadKey();
+Task.WaitAll(task1, task2);
 
 Console.WriteLine(i);
---
+++ System Programming/MutexApp/Program.cs
@@ -0,0 +1,14 @@
+﻿Mutex mutex = new Mutex(false, name: "MyMutex");
+
+Console.WriteLine("Start App");
+
+mutex.WaitOne();
+
+Console.WriteLine("Open Mutex");
+Console.ReadKey();
+
+mutex.ReleaseMutex();
+
+Console.WriteLine("End App");
+
+Console.ReadKey();
