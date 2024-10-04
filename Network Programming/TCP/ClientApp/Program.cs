9923e788bf5047bca50b0fcb7ce3efe49aefa42f
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2024-10-04 21:37:53 +0400

    Add TCP ClientServer app

---
+++ Network Programming/TCP/ClientApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int serverPort = 7070;
+IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+TcpClient tcpClient = new TcpClient();
+
+await tcpClient.ConnectAsync(new IPEndPoint(ipAddress, serverPort));
+
+var tcpClientStream = tcpClient.GetStream();
+var streamReader = new StreamReader(tcpClientStream);
+
+var messageFromServer = await streamReader.ReadLineAsync();
+
+//byte[] buffer = new byte[1024];
+//var size = tcpClientStream.Read(buffer, 0, buffer.Length);
+//var messageFromServer = Encoding.UTF8.GetString(buffer, 0, size);
+
+Console.WriteLine($"Server: '{messageFromServer}'");
---
+++ Network Programming/TCP/ServerApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+const int serverPort = 7070;
+IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+TcpListener tcpListener = new TcpListener(ipAddress, serverPort);
+
+tcpListener.Start();
+Console.WriteLine($"server started on port '{serverPort}'...");
+
+while(true)
+{
+    var tcpClient = await tcpListener.AcceptTcpClientAsync();
+    Console.WriteLine("client accepted!");
+
+    if(tcpClient.Client.RemoteEndPoint is IPEndPoint tcpClientIpEndpoint)
+    {
+        Console.WriteLine($"Client connected with port '{tcpClientIpEndpoint.Port}'");
+    }
+
+    var tcpClientStream = tcpClient.GetStream();
+
+    var streamWriter = new StreamWriter(tcpClientStream);
+
+    Thread.Sleep(5000);
+
+    await streamWriter.WriteLineAsync("Welcome to server!");
+    await streamWriter.FlushAsync();
+
+    //var welcomeMessage = "Welcome to server!";
+    //var welcomeMessageInBytes = Encoding.UTF8.GetBytes(welcomeMessage);
+    //await tcpClientStream.WriteAsync(welcomeMessageInBytes);
+    //await tcpClientStream.FlushAsync();
+}
