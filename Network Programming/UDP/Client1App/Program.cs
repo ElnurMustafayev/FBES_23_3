29526af2efeb71321d9d509758a73736ea5951a3
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2024-10-13 13:00:22 +0400

    Add UDP package limits

--- Network Programming/UDP/Client1App/Program.cs
+++ Network Programming/UDP/Client1App/Program.cs
@@ -1,8 +1,64 @@
 ﻿using System.Net.Sockets;
 using System.Text;
 
+const int udpPackageSize = 10_000;
+const string eofKey = "end-of-file";
+var eofInBytes = Encoding.UTF8.GetBytes(eofKey);
+
 var udpClient = new UdpClient(7070);
 
+while(true) {
+    try {
+        Console.Clear();
+        System.Console.Write("Input file name: ");
+        var filename = Console.ReadLine();
+        ArgumentNullException.ThrowIfNullOrWhiteSpace(filename, nameof(filename));
+
+        var filepath = $"Assets/{filename}";
+
+        if(File.Exists(filepath) == false) {
+            throw new ArgumentException($"File '{filepath}' not found!");
+        }
+
+        var fileInBytes = await File.ReadAllBytesAsync(filepath);
+
+        // package logic
+        for (int packageCounter = 0; true; packageCounter++)
+        {
+            // if(packageCounter > 3 && packageCounter < 8) {
+            //     continue;
+            // }
+
+            var fileInBytesPackage = fileInBytes
+                .Skip(udpPackageSize * packageCounter)
+                .Take(udpPackageSize).ToArray();
+
+            // end of file
+            if(fileInBytesPackage.Any() == false) {
+                break;
+            }
+
+            //Thread.Sleep(1000);
+            await udpClient.SendAsync(fileInBytesPackage, "localhost", 8282);
+            System.Console.WriteLine($"Package {packageCounter + 1} sent! lenght: {fileInBytesPackage.Length}");
+        }
+        
+        //Thread.Sleep(1000);
+        await udpClient.SendAsync(eofInBytes, "localhost", 8282);
+        System.Console.WriteLine($"File {filename} sent!");
+        Console.ReadKey();
+    }
+    catch(Exception ex) {
+        System.Console.WriteLine($"{ex.GetType().Name}: {ex.Message}");
+        System.Console.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+}
+
+
+
+
+/*
 while(true) {
     var message = Console.ReadLine();
 
@@ -13,3 +69,4 @@ while(true) {
 
     await udpClient.SendAsync(messageInBytes, "localhost", 8282);
 }
+*/
--- Network Programming/UDP/Client2App/Program.cs
+++ Network Programming/UDP/Client2App/Program.cs
@@ -1,8 +1,45 @@
 ﻿using System.Net.Sockets;
 using System.Text;
 
+const string eofKey = "end-of-file";
+const int port = 8282;
+
 var udpClient = new UdpClient(8282);
 
+System.Console.WriteLine($"UDP client starting receivig on port: {port}");
+
+while(true) {
+    var buffer = new List<byte>();
+
+    for (int packageCounter = 0; true; packageCounter++)
+    {
+        var result = await udpClient.ReceiveAsync();
+
+        System.Console.WriteLine($"Package {packageCounter + 1} received from {result.RemoteEndPoint}");
+        System.Console.WriteLine($"Package size: {result.Buffer.Length} bytes");
+
+        var fileInBytes = result.Buffer;
+
+        if(fileInBytes.Length == eofKey.Length && Encoding.UTF8.GetString(fileInBytes) == eofKey) {
+            break;
+        }
+
+        buffer.AddRange(fileInBytes);
+    }
+
+    var filename = $"{DateTime.Now.ToString("ddMMyyyyhhmmss")}_{Guid.NewGuid().ToString("N")[..5]}.jpg";
+    var filepath = $"Downloads/{filename}";
+    
+    await File.WriteAllBytesAsync(
+        path: filepath,
+        bytes: buffer.ToArray()
+    );
+    System.Console.WriteLine($"File created: {filepath}");
+}
+
+
+
+/*
 while(true) {
     var result = await udpClient.ReceiveAsync();
 
@@ -10,3 +47,4 @@ while(true) {
 
     System.Console.WriteLine($"{result.RemoteEndPoint}: {message}"); 
 }
+*/
