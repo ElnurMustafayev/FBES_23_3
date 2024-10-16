934c26c7fc4acb5088a9babae9ad370f3c6a34d9
Elnur Mustafayev <elnur.mustafayeev@gmail.com>
2024-10-16 22:02:30 +0400

    Add HTTP Client

--- Network Programming/HTTP/ClientApp/Program.cs
+++ Network Programming/HTTP/ClientApp/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Json;
+
+HttpClient httpClient = new HttpClient();
+
+if(true) {
+    var response = await httpClient.PostAsJsonAsync("http://localhost:7373/account/login", new {
+        username = "bob",
+        password = "secret12345"
+    });
+
+    var status = response.StatusCode;
+    var responseBodyStr = await response.Content.ReadAsStringAsync();
+
+    System.Console.WriteLine($"Response Status: {status}");
+    System.Console.WriteLine($"Response Body: {responseBodyStr}");
+}
+
+if(false) {
+    System.Console.Write("User id: ");
+    var userIdStr = Console.ReadLine();
+
+    ArgumentNullException.ThrowIfNullOrWhiteSpace(userIdStr);
+
+    if(int.TryParse(userIdStr, out int userId)) {
+        HttpResponseMessage response = await httpClient.GetAsync($"http://localhost:7373/users/{userId}");
+
+        var status = response.StatusCode;
+        var responseBodyStr = await response.Content.ReadAsStringAsync();
+
+        System.Console.WriteLine($"Response Status: {status}");
+        System.Console.WriteLine($"Response Body: {responseBodyStr}");
+    }
+    else {
+        System.Console.WriteLine("Incorrect input! User id is number!");
+    }
+}
--- Network Programming/HTTP/ServerApp/Program.cs
+++ Network Programming/HTTP/ServerApp/Program.cs
@@ -11,28 +11,88 @@ httpListener.Start();
 
 System.Console.WriteLine($"HTTP Server started on 'http://localhost:{port}/'");
 
+
 while(true) {
+
+    // READ REQUEST BODY
+    var context = await httpListener.GetContextAsync();
+
+    var reader = new StreamReader(context.Request.InputStream);
+
+    var requestBodyStr = await reader.ReadToEndAsync();
+
+    System.Console.WriteLine("RawUrl: " + context.Request.RawUrl);
+    System.Console.WriteLine("ContentType: " + context.Request.ContentType);
+    System.Console.WriteLine("HttpMethod: " + context.Request.HttpMethod);
+    System.Console.WriteLine("requestBodyStr: " + requestBodyStr);
+
+    var writer = new StreamWriter(context.Response.OutputStream);
+    await writer.WriteLineAsync("IT IS OK!");
+    await writer.FlushAsync();
+
+    context.Response.Close();
+
+
+
+
+
+
+/* ROUTING
     var context = await httpListener.GetContextAsync();
 
     var writer = new StreamWriter(context.Response.OutputStream);
 
     System.Console.WriteLine(context.Request.RawUrl);
-    System.Console.WriteLine(context.Request.ContentType);
-    context.Response.ContentType = "text/plain";
+    //System.Console.WriteLine(context.Request.ContentType);
+    context.Response.ContentType = "application/json";
 
-    await writer.WriteLineAsync(await File.ReadAllTextAsync("Pages/index.html"));
+    //await writer.WriteLineAsync(await File.ReadAllTextAsync("Pages/index.html"));
 
-    // var json = JsonSerializer.Serialize(new {
-    //     name = "Bob",
-    //     surname = "Marley",
-    //     birthdate = new DateTime(1954, 2, 2),
-    // });
+    var normalizedRawUrl = context.Request.RawUrl?.Trim().ToLower() ?? "/";
+    var rawUrlItems = normalizedRawUrl.Split("/", StringSplitOptions.RemoveEmptyEntries);
 
-    //await writer.WriteLineAsync(json);
-    await writer.FlushAsync();
+    if(rawUrlItems.Length == 0) {
+        await writer.WriteLineAsync("<h1>Welcome</h1>");
+        await writer.FlushAsync();
+
+        context.Response.StatusCode = (int)HttpStatusCode.OK;
+    }
+    else if(rawUrlItems[0] == "users") {
+
+        if(context.Request.HttpMethod == HttpMethod.Get.Method) {
+
+            if(rawUrlItems.Length == 2) {
+                if(int.TryParse(rawUrlItems[1], out int userId)) {
+                    var json = JsonSerializer.Serialize(new {
+                        id = userId,
+                        name = "Bob",
+                        surname = "Marley",
+                        birthdate = new DateTime(1954, 2, 2),
+                    });
+
+                    await writer.WriteLineAsync(json);
+                    await writer.FlushAsync();
+
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                }
+                else {
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                }
+            }
+            else {
+                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            }
+        }
+        else {
+            context.Response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+        }
+    }
+    else {
+        context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+    }
 
     context.Response.Close();
-
+*/
 
 
 
