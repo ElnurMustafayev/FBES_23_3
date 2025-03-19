59ef820b20aea108122c07f43039605d1a45f6a4
Mustafayev Elnur Ilham <mustafayev_e@itstep.edu.az>
2025-03-19 19:56:46 +0400

    Add unit test methods naming convention

--- Tests/Unit Tests/test/MyApp.WebApi.Tests/Services/CalcServiceTests.cs
+++ Tests/Unit Tests/test/MyApp.WebApi.Tests/Services/CalcServiceTests.cs
@@ -12,7 +12,7 @@ public class CalcServiceTests
     }
 
     [Fact]
-    public void Tests1() {
+    public void Sum_SumOfTwoPositiveNumbers_CorrectSumResult() {
         var result = this.service.Sum(5, 2);
         Assert.Equal(7, result);
     }
@@ -22,13 +22,13 @@ public class CalcServiceTests
     [InlineData(0, 0, 0)]
     [InlineData(-3, -4, -7)]
     [InlineData(-3, 10, 7)]
-    public void Tests2(decimal num1, decimal num2, decimal expectedResult) {
+    public void Sum_SumOfTwoNumbers_CorrectSumResult(decimal num1, decimal num2, decimal expectedResult) {
         var result = this.service.Sum(num1, num2);
         Assert.Equal(expectedResult, result);
     }
 
     [Fact]
-    public void Tests3() {
+    public void Divide_DivideByZero_ThrowArgumentException() {
         Assert.Throws<ArgumentException>(() => this.service.Divide(5, 0));
     }
 }
